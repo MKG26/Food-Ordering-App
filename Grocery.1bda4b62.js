b8dd282e945024d075bb7803a295e96f292dc529
Mohit <92150824+MKG26@users.noreply.github.com>
2024-09-03 18:18:07 +0530

    Updates

---
+++ Grocery.1bda4b62.js
@@ -0,0 +1,2 @@
+var e;(0,(e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire77dd).register)("9wveP",function(n,i){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return r},set:void 0,enumerable:!0,configurable:!0});var o=e("iD4WH"),r=function(){return(0,o.jsx)("h1",{children:"This is Grocery section"})}});
+//# sourceMappingURL=Grocery.1bda4b62.js.map
