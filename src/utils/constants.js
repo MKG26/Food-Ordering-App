455c18de91c1682301474382d7ba599eec27adb0
Mohit <92150824+MKG26@users.noreply.github.com>
2024-09-04 01:03:29 +0530

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 export const IMG_CDN_URL =
-  "https://your-netlify-site.netlify.app/.netlify/functions/cors?url=https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+  "https://thingproxy.freeboard.io/fetch/https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4iVUNS_R59D9G0s-kYJmgV9CI_j-X6WExog&s";
 
 export const MENU_ITEM =
-  "https://your-netlify-site.netlify.app/.netlify/functions/cors?url=https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5355161&lng=77.3910265&restaurantId=";
 
 export const MENU_ITEM_IMG =
-  "https://your-netlify-site.netlify.app/.netlify/functions/cors?url=https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+  "https://thingproxy.freeboard.io/fetch/https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
